3b664a629fd7821d765eae090ac6a98b271009da
Mazharul <mazharul.belal@gmail.com>
2025-11-10 20:46:04 +0600

    code update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UnityAds",
-            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.16.3/UnityAds.xcframework.zip",
+            url: "https://github.com/mazharulbelal/ads/releases/download/4.16.3/UnityAds.xcframework.zip",
             checksum: "d1f5a008e85ac068d15c176845378ff4d98f4b719e27c3935d3a39ba428b3843"
         )
     ]
